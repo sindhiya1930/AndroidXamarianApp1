bc60ee89ac051acd39849d52ad85da72c9dabdea
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2017-10-22 21:43:52 -0400

    following https://www.youtube.com/watch?v=NDIoRgGFUNI&feature=youtu.be

--- AndroidXamarianApp1/MainActivity.cs
+++ AndroidXamarianApp1/MainActivity.cs
@@ -7,12 +7,24 @@ namespace AndroidXamarianApp1
     [Activity(Label = "AndroidXamarianApp1", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        int count = 1;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource.
+            // and attach an event to it.
+            Button button = FindViewById<Button>(Resource.);
+
+            button.Click += delegate
+            {
+                button.Text = string.Format("{0} clicks!", count++);
+            };
+
         }
     }
 }
